0a70abca2dee013c88725070003d66573d020f96
VARUN KUMAR GOLLAVILLI <138124334+varun-aiml@users.noreply.github.com>
2025-07-12 19:12:43 +0530

    Update passport.js

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -21,7 +21,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:5000/api/auth/google/callback',
+      callbackURL: 'https://sportstek.onrender.com/api/auth/google/callback',
       scope: ['profile', 'email']
     },
 
